4607391d1fb81e754b3bbf79a9de4497f58c38e3
Lance Ball <lball@redhat.com>
2016-11-03 17:07:38 -0400

    docs: Document API and add an API publish script

--- index.js
+++ index.js
@@ -10,10 +10,43 @@ const defaults = {
   Promise: Fidelity
 };
 
+/**
+ * @module opossum
+ */
+
+/**
+ * Creates a {@link CircuitBreaker} instance capable of executing `action`.
+ * @param action {function} The action to fire for this {@link CircuitBreaker} instance
+ * @param options {Object} Options for the {@link CircuitBreaker}
+ * @param options.timeout {number} The time in milliseconds that action should
+ * be allowed to execute before timing out. Default 10000 (10 seconds)
+ * @param options.maxFailures The number of times the circuit can fail before
+ * opening. Default 10.
+ * @param options.resetTimeout The time in milliseconds to wait before setting
+ * the breaker to `halfOpen` state, and trying the action again.
+ * @param options.Promise {Promise} Opossum uses Fidelity promises, but works
+ * fine with any Promise that follows the spec. You can specify your favored
+ * implementation by providing the constructor as an option.
+ * @return a {@link CircuitBreaker} instance
+ */
 function circuitBreaker (action, options) {
   return new CircuitBreaker(action, Object.assign({}, defaults, options));
 }
 
+/**
+ * Given a function that receives a callback as its last argument,
+ * and which executes that function, passing as parameters `err` and `result`,
+ * creates an action that returns a promise which resolves when the function's
+ * callback is executed.
+ * @function promisify
+ *
+ * @param action {function} A Node.js-like asynchronous function
+ * @return The `action` wrapped in a promise API.
+ * @example
+ *     const fs = require('fs');
+ *     const readFilePromised = circuitBreaker.promisify(fs.readFile);
+ *     const breaker = circuitBreaker(readFilePromised);
+ */
 circuitBreaker.promisify = require('./lib/promisify');
 
 module.exports = exports = circuitBreaker;
---
+++ publish-docs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+PUBLISH_DIR="publish"
+
+VERSION="0.14.0"
+echo $1
+git status -s | grep "^[MADRCU\? ][ MDMAU\?]"
+if [[ $? -eq 0 && "$1" != "-f" ]]; then
+  echo "You have changes in your working directory. Take care of that first, 'k?"
+  echo "Override this, with the '-f' flag."
+  exit 1;
+fi
+
+while true; do
+  read -p "This will publish the API docs. Continue? [Y/n] " yn
+  case $yn in
+    [Nn]* ) echo "Aborting."; exit;;
+    * ) echo "Continuing."; break;;
+  esac
+done
+
+npm run docs
+git clone https://github.com/bucharest-gold/opossum.git $PUBLISH_DIR
+cd $PUBLISH_DIR
+git checkout gh-pages
+mkdir $VERSION
+rsync -r ../docs/* ./
+rsync -r ../docs/* ./$VERSION/
+git status -s | grep "^[MADRCU\? ][ MDMAU\?]"
+
+if [ $? -eq 0 ]; then
+  git add .
+  echo "Commiting and publishing"
+  git commit -a -m "Update docs"
+  git push origin gh-pages
+else
+  echo "No changes to publish"
+fi
+
+cd ..
+rm -rf $PUBLISH_DIR
