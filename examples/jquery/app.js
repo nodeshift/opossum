048b02f5572921bdfda88b4f6f56f76e50559f2b
Lucas Holmquist <lholmqui@redhat.com>
2017-03-03 12:51:47 -0500

    enhancement: prepend the circuit events to see the most recent event first (#22)

--- examples/jquery/app.js
+++ examples/jquery/app.js
@@ -4,7 +4,7 @@
 (function appInitialization () {
   $(() => {
     $('#flakey').click(() => circuit.fire().catch((e) => console.error(e)));
-    $('.clear').click(function () { $(this).siblings('p').remove(); });
+    $('.clear').click(function () { $(this).parent().find('#flakeyResponse').remove(); });
   });
 
   const route = '/flakeyService';
@@ -21,31 +21,31 @@
   circuit.fallback(() => ({ body: `${route} unavailable right now. Try later.` }));
 
   circuit.on('success',
-    (result) => $(element).append(
+    (result) => $(element).prepend(
       makeNode(`SUCCESS: ${JSON.stringify(result)}`)));
 
   circuit.on('timeout',
-    () => $(element).append(
+    () => $(element).prepend(
       makeNode(`TIMEOUT: ${route} is taking too long to respond.`)));
 
   circuit.on('reject',
-    () => $(element).append(
+    () => $(element).prepend(
       makeNode(`REJECTED: The breaker for ${route} is open. Failing fast.`)));
 
   circuit.on('open',
-    () => $(element).append(
+    () => $(element).prepend(
       makeNode(`OPEN: The breaker for ${route} just opened.`)));
 
   circuit.on('halfOpen',
-    () => $(element).append(
+    () => $(element).prepend(
       makeNode(`HALF_OPEN: The breaker for ${route} is half open.`)));
 
   circuit.on('close',
-    () => $(element).append(
+    () => $(element).prepend(
       makeNode(`CLOSE: The breaker for ${route} has closed. Service OK.`)));
 
   circuit.on('fallback',
-    (data) => $(element).append(
+    (data) => $(element).prepend(
       makeNode(`FALLBACK: ${JSON.stringify(data)}`)));
 
   function makeNode (body) {
