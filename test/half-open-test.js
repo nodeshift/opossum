5fe38945ea09bfbb07407ac975a2d52c64a262f6
Lance Ball <lball@redhat.com>
2017-12-13 16:05:52 -0500

    test: improve timing of half-open test

--- test/half-open-test.js
+++ test/half-open-test.js
@@ -5,38 +5,35 @@ const opossum = require('../');
 const { timedFailingFunction } = require('./common');
 
 test('When half-open, the circuit only allows one request through', t => {
-  t.plan(12);
+  t.plan(10);
   const options = {
     errorThresholdPercentage: 1,
-    resetTimeout: 500
+    resetTimeout: 100
   };
 
   const breaker = opossum(timedFailingFunction, options);
   breaker.fire(1)
     .catch((e) => t.equals(e, 'Failed after 1'))
     .then(() => {
-      t.ok(breaker.opened, 'breaker should be open');
-      t.notOk(breaker.pendingClose, 'breaker should not be pending close');
-    })
-    .then(() => {
-      setTimeout(() => {
-        t.ok(breaker.halfOpen, 'breaker should be halfOpen');
-        t.ok(breaker.pendingClose, 'breaker should be pending close');
-        breaker
-          .fire(500) // fail after a long time, letting possibly other fire()s to occur
-          .catch((e) => t.equals(e, 'Failed after 500', 'function should fail again'))
-          .then(() => {
-            t.ok(breaker.opened, 'breaker should be open again');
-            t.notOk(breaker.halfOpen, 'breaker should not be halfOpen');
-            t.notOk(breaker.pendingClose, 'breaker should not be pending close');
-          });
-      }, options.resetTimeout * 1.1);
+      t.ok(breaker.opened, 'breaker should be open after initial fire');
+      t.notOk(breaker.pendingClose, 'breaker should not be pending close after initial fire');
     });
-  // fire the breaker again, and be sure it fails as expected
-  breaker.fire(1)
-    .catch((e) => t.equals(e, 'Failed after 1'))
-    .then(() => {
-      t.ok(breaker.opened, 'breaker should be open');
-      t.notOk(breaker.pendingClose, 'breaker should not be pending close');
-    });
+
+  // Fire again after reset timeout. should be half open
+  setTimeout(() => {
+    t.ok(breaker.halfOpen, 'breaker should be halfOpen after timeout');
+    t.ok(breaker.pendingClose, 'breaker should be pending close after timeout');
+    breaker
+      .fire(500) // fail after a long time, letting possibly other fire()s to occur
+      .catch((e) => t.equals(e, 'Failed after 500', 'function should fail again'))
+      .then(() => {
+        t.ok(breaker.opened, 'breaker should be open again after long failing function');
+        t.notOk(breaker.halfOpen, 'breaker should not be halfOpen after long failing function');
+        t.notOk(breaker.pendingClose, 'breaker should not be pending close after long failing function');
+      });
+    // fire the breaker again, and be sure it fails as expected
+    breaker
+      .fire(1)
+      .catch((e) => t.equals(e.message, 'Breaker is open'));
+  }, options.resetTimeout * 1.1);
 });
