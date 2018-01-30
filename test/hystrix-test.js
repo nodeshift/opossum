b63569b431f38da3c05ab133d3fea3005ab94a34
Lance Ball <lball@redhat.com>
2018-01-29 19:40:55 -0500

    feat: allow multiple circuits to aggregate stats
    If a user creates multiple circuits in the same process, they should aggregate
    their stats in the Hystrix stream.
    
    Fixes: https://github.com/bucharest-gold/opossum/issues/125
    Ref: https://github.com/bucharest-gold/opossum/issues/124

---
+++ test/hystrix-test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const test = require('tape');
+const cb = require('../');
+const { passFail } = require('./common');
+
+test('A circuit should provide stats to a hystrix compatible stream', t => {
+  t.plan(2);
+  const circuitOne = cb(passFail, {
+    rollingCountTimeout: 100,
+    rollingCountBuckets: 1,
+    name: 'circuit one'
+  });
+  const circuitTwo = cb(passFail, {
+    rollingCountTimeout: 100,
+    rollingCountBuckets: 1,
+    name: 'circuit two'
+  });
+  const stream = circuitOne.hystrixStats.getHystrixStream();
+  let circuitOneStatsSeen = false;
+  let circuitTwoStatsSeen = true;
+  stream.on('data', blob => {
+    const obj = JSON.parse(blob);
+    if (obj.data.name === 'circuit one') circuitOneStatsSeen = true;
+    else if (obj.data.name === 'circuit two') circuitTwoStatsSeen = true;
+  });
+  circuitOne.fire(10).then(_ => circuitTwo.fire(10)).then(_ => {
+    console.log('checking');
+    t.ok(circuitOneStatsSeen, 'circuit one stats seen');
+    t.ok(circuitTwoStatsSeen, 'circuit two stats seen');
+    t.end();
+  });
+});
