016eba5fc6279b7bba882b9e4b65efd1c0c9b3a9
Lance Ball <lball@redhat.com>
2018-03-23 14:49:17 -0400

    feat: add enable/disable for a circuit (#160)
    This commit also includes some code climate refactoring. 
    
    Fixes: https://github.com/bucharest-gold/opossum/issues/136

---
+++ test/enable-disable-test.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const test = require('tape');
+const opossum = require('../');
+const { passFail } = require('./common');
+
+test('Defaults to enabled', t => {
+  t.plan(1);
+  const breaker = opossum(passFail);
+  t.equals(breaker.enabled, true);
+  t.end();
+});
+
+test('Accepts options.enabled', t => {
+  t.plan(1);
+  const breaker = opossum(passFail, { enabled: false });
+  t.equals(breaker.enabled, false);
+  t.end();
+});
+
+test('When disabled the circuit should always be closed', t => {
+  t.plan(8);
+  const options = {
+    errorThresholdPercentage: 1,
+    resetTimeout: 1000
+  };
+
+  const breaker = opossum(passFail, options);
+  breaker.disable();
+  breaker.fire(-1)
+    .catch(e => t.equals(e, 'Error: -1 is < 0'))
+    .then(() => {
+      t.ok(breaker.closed, 'should be closed');
+      t.notOk(breaker.pendingClose,
+        'should not be pending close');
+    })
+    .then(() => {
+      breaker // fire and fail again
+        .fire(-1)
+        .catch(e => t.equals(e, 'Error: -1 is < 0'))
+        .then(() => {
+          t.ok(breaker.closed, 'should be closed');
+          t.notOk(breaker.pendingClose,
+            'should not be pending close');
+        });
+    })
+    .then(() => { // reenable the circuit
+      breaker.enable();
+      breaker.fire(-1)
+        .catch(e => t.equals(e, 'Error: -1 is < 0'))
+        .then(() => {
+          t.ok(breaker.opened, 'should be closed');
+        });
+    });
+});
