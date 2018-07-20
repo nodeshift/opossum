2fea233d3a9701ffeabc5bf353cd13df39837888
Lance Ball <lball@redhat.com>
2018-07-20 16:04:15 -0400

    feat: add options.allowWarmUp as a creation option
    This feature allows the user to let the circuit warm up
    before opening, even if every request is a failure or timeout.
    The warmup duration is the value provided for
    options.rollingCountTimeout.
    
    Fixes: https://github.com/bucharest-gold/opossum/issues/217

---
+++ test/warmup-test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const test = require('tape');
+const opossum = require('../');
+const { passFail } = require('./common');
+
+test('By default does not allow for warmup', t => {
+  t.plan(3);
+  const options = {
+    errorThresholdPercentage: 1,
+    resetTimeout: 100
+  };
+
+  const breaker = opossum(passFail, options);
+  breaker.fire(-1)
+    .catch(e => t.equals(e, 'Error: -1 is < 0'))
+    .then(() => {
+      t.ok(breaker.opened, 'should be open after initial fire');
+      t.notOk(breaker.pendingClose,
+        'should not be pending close after initial fire');
+    });
+});
+
+test('Allows for warmup when option is provided', t => {
+  t.plan(3);
+  const options = {
+    errorThresholdPercentage: 1,
+    resetTimeout: 100,
+    allowWarmUp: true
+  };
+
+  const breaker = opossum(passFail, options);
+  breaker.fire(-1)
+    .catch(e => t.equals(e, 'Error: -1 is < 0'))
+    .then(() => {
+      t.notOk(breaker.opened, 'should not be open after initial fire');
+      t.notOk(breaker.pendingClose,
+        'should not be pending close after initial fire');
+    });
+});
+
+test('Only warms up for rollingCountTimeout', t => {
+  t.plan(4);
+  const options = {
+    errorThresholdPercentage: 1,
+    resetTimeout: 100,
+    allowWarmUp: true,
+    rollingCountTimeout: 500
+  };
+
+  const breaker = opossum(passFail, options);
+  breaker.fire(-1)
+    .catch(e => t.equals(e, 'Error: -1 is < 0'))
+    .then(() => {
+      t.notOk(breaker.opened, 'should not be open after initial fire');
+      t.notOk(breaker.pendingClose,
+        'should not be pending close after initial fire');
+    })
+    .then(() => {
+      setTimeout(_ => {
+        t.notOk(breaker.warmUp, 'Warmup should end after rollingCountTimeout');
+      }, 500);
+    });
+});
