ff42d1b089c2da9f9d3e0a55e4ea5a23a7168232
Lance Ball <lball@redhat.com>
2018-08-02 16:00:58 -0400

    feat: add options.allowWarmUp as a creation option (#218)
    This feature allows the user to let the circuit warm up
    before opening, even if every request is a failure or timeout.
    The warmup duration is the value provided for
    options.rollingCountTimeout.
    
    Fixes: https://github.com/bucharest-gold/opossum/issues/217

--- index.js
+++ index.js
@@ -22,6 +22,30 @@ const defaults = {
  * opening. Default 10.
  * @param options.resetTimeout The time in milliseconds to wait before setting
  * the breaker to `halfOpen` state, and trying the action again.
+ * @param options.rollingCountTimeout Sets the duration of the statistical rolling
+ * window, in milliseconds. This is how long Opossum keeps metrics for the circuit
+ * breaker to use and for publishing. Default: 10000
+ * @param options.rollingCountBuckets Sets the number of buckets the rolling
+ * statistical window is divided into. So, if options.rollingCountTimeout is
+ * 10000, and options.rollingCountBuckets is 10, then the statistical window will
+ * be 1000 1 second snapshots in the statistical window. Default: 10
+ * @param options.name the circuit name to use when reporting stats
+ * @param options.rollingPercentilesEnabled This property indicates whether
+ * execution latencies should be tracked and calculated as percentiles. If they
+ * are disabled, all summary statistics (mean, percentiles) are returned as -1.
+ * Default: false
+ * @param options.capacity the number of concurrent requests allowed. If the number
+ * currently executing function calls is equal to options.capacity, further calls
+ * to `fire()` are rejected until at least one of the current requests completes.
+ * @param options.errorThresholdPercentage the error percentage at which to open the
+ * circuit and start short-circuiting requests to fallback.
+ * @param options.enabled whether this circuit is enabled upon construction. Default: true
+ * @param options.allowWarmUp {boolean} determines whether to allow failures
+ * without opening the circuit during a brief warmup period (this is the
+ * `rollingCountDuration` property). Default: false
+ * allow before enabling the circuit. This can help in situations where no matter
+ * what your `errorThresholdPercentage` is, if the first execution times out or
+ * fails, the circuit immediately opens. Default: 0
  * @return a {@link CircuitBreaker} instance
  */
 function circuitBreaker (action, options) {
