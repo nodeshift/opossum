919f8c84109fab2a42047161644c30a109f8c620
Lance Ball <lball@redhat.com>
2019-01-25 17:14:11 -0500

    doc: update jquery example to latest

--- examples/jquery/index.js
+++ examples/jquery/index.js
@@ -4,76 +4,76 @@ const Hapi = require('hapi');
 const Boom = require('boom');
 const path = require('path');
 
-const server = new Hapi.Server();
-
-server.connection({
+const server = Hapi.Server({
   host: 'localhost',
   port: 3000
 });
 
-// static file serving
-server.register(require('inert', err => possibleError(err)));
+async function start() {
+  // static file serving
+  await server.register({ plugin: require('inert') });
 
-[ ['/', path.join(__dirname, 'index.html')],
+  [ ['/', path.join(__dirname, 'index.html')],
   ['/app.js', path.join(__dirname, 'app.js')],
   ['/jquery.js',
     path.join(__dirname, 'node_modules', 'jquery', 'dist', 'jquery.js')],
-  ['/opossum.js', path.join(__dirname, '..', '..', 'dist', 'opossum.js')]
-].map(entry => {
-  server.route({
-    method: 'GET',
-    path: entry[0],
-    handler: {
-      file: {
-        path: entry[1],
-        confine: false
+  ['/opossum.js', path.join(__dirname, 'node_modules', 'opossum', 'dist', 'opossum.js')]
+  ].map(entry => {
+    server.route({
+      method: 'GET',
+      path: entry[0],
+      handler: (request, h) => {
+        console.error(`looking for ${entry[1]}`);
+        return h.file(entry[1])
       }
-    }
-  });
-});
-
-const baseline = 20;
-let delay = baseline;
-server.route({
-  method: 'GET',
-  path: '/flakeyService',
-  handler: function flakeyService (request, reply) {
-    console.log('Flakey service delay', delay);
-    // if we're really slowing down, just reply with an error
-    if (delay > 1000) {
-      console.log('Long delay encountered, returning Error 423 (Locked)');
-      return reply(Boom.locked('Flakey service is flakey'));
-    }
-    const response = reply({
-      body: 'Flakey service response',
-      delay
-    }, delay).hold();
-    setTimeout(() => {
-      console.log('Replying with flakey response after delay of', delay);
-      delay = delay * 2;
-      response.send();
-    }, delay);
-  }
-});
-
-// reset the delay every 10 seconds
-setInterval(() => {
-  if (delay !== baseline) {
-    delay = baseline;
-    console.log('Resetting flakey service delay to', delay);
-  }
-}, 20000);
-
-server.start(err => {
-  possibleError(err);
-  console.log(`Server: ${server.info.uri}`);
-  console.log('Endpoints:');
-  server.table().map(entry => {
-    entry.table.map(route => {
-      console.log(`${route.method} ${route.path}`);
     });
   });
-});
+
+  const baseline = 20;
+  let delay = baseline;
+  server.route({
+    method: 'GET',
+    path: '/flakeyService',
+    handler: function flakeyService (request, h) {
+      console.log('Flakey service delay', delay);
+      // if we're really slowing down, just reply with an error
+      if (delay > 1000) {
+        console.log('Long delay encountered, returning Error 423 (Locked)');
+        return Boom.locked('Flakey service is flakey');
+      }
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log('Replying with flakey response after delay of', delay);
+          delay = delay * 2;
+          resolve({
+            body: 'Flakey service response',
+            delay        })
+        }, delay);
+      });
+    }
+  });
+
+  // reset the delay every 10 seconds
+  setInterval(() => {
+    if (delay !== baseline) {
+      delay = baseline;
+      console.log('Resetting flakey service delay to', delay);
+    }
+  }, 20000);
+
+
+  await server.start(err => {
+    possibleError(err);
+    console.log(`Server: ${server.info.uri}`);
+    console.log('Endpoints:');
+    server.table().map(entry => {
+      entry.table.map(route => {
+        console.log(`${route.method} ${route.path}`);
+      });
+    });
+  });
+  
+}
 
 process.on('uncaughtException', e => {
   process._rawDebug(`Uncaught exception ${e}`);
@@ -82,3 +82,5 @@ process.on('uncaughtException', e => {
 function possibleError (err) {
   if (err) throw err;
 }
+
+start();
