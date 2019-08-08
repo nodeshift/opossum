87cb8f5fd645f137d36bfe8af7568298ca530f9c
Lance Ball <lball@redhat.com>
2019-08-08 08:43:01 -0400

    src: allow HystrixStream to accept circuits asynchronously

--- test/hystrix-test.js
+++ test/hystrix-test.js
@@ -32,3 +32,32 @@ test('A circuit should provide stats to a hystrix compatible stream', t => {
     t.end();
   });
 });
+
+test('Hystrix stats should accept additional circuits', t => {
+  t.plan(2);
+  const circuitOne = cb(passFail, {
+    rollingCountTimeout: 100,
+    rollingCountBuckets: 1,
+    name: 'circuit one'
+  });
+  const circuitTwo = cb(passFail, {
+    rollingCountTimeout: 100,
+    rollingCountBuckets: 1,
+    name: 'circuit two'
+  });
+  const hystrixStats = new HystrixStats([circuitOne]);
+  hystrixStats.add(circuitTwo);
+  const stream = hystrixStats.getHystrixStream();
+  let circuitOneStatsSeen = false;
+  let circuitTwoStatsSeen = false;
+  stream.on('data', blob => {
+    const obj = JSON.parse(blob.substring(6));
+    if (obj.name === 'circuit one') circuitOneStatsSeen = true;
+    else if (obj.name === 'circuit two') circuitTwoStatsSeen = true;
+  });
+  circuitOne.fire(10).then(_ => circuitTwo.fire(10)).then(_ => {
+    t.ok(circuitOneStatsSeen, 'circuit one stats seen');
+    t.ok(circuitTwoStatsSeen, 'circuit two stats seen');
+    t.end();
+  });
+});
