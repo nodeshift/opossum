401935b73be7be7e18dc04604a1c1acb7f9cb2b2
Tom Vogel <tom.vogel@appdirect.com>
2019-10-21 10:49:32 -0700

    Implement babel-loader in webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -58,7 +58,26 @@ function generateConfig (name) {
         }
       })
     ],
-    devtool: 'source-map'
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {
+                  'useBuiltIns': 'usage',
+                  'corejs': 3
+                }]
+              ]
+            }
+          }
+        }
+      ]
+    }
   };
   return config;
 }
