03d0b6642c901079b295c2221cfb5d06f3897637
Jair Batista <jair.batista@maxmilhas.com.br>
2019-12-02 19:33:05 -0300

    Fix #388 - Circuit stuck halfOpen
    When an error is filtered by an errorFilter the request is
    considered neither success nor failure, leaving the circuit
    in an inconsistent state. This fixes that issue by emitting
    success event when the error is filtered.

--- test/half-open-test.js
+++ test/half-open-test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const test = require('tape');
-const CircuitBreaker = require('../');
+const CircuitBreaker = require('../lib/circuit');
 const { timedFailingFunction, failWithCode } = require('./common');
 
 test('When half-open, the circuit only allows one request through', t => {
@@ -47,7 +47,7 @@ test('When half-open, the circuit only allows one request through', t => {
   }, options.resetTimeout * 1.5);
 });
 
-test('When half-open, a fail with a filtered error', t => {
+test('When half-open, a filtered error should close the circuit', t => {
   t.plan(7);
   const options = {
     errorThresholdPercentage: 1,
@@ -63,21 +63,22 @@ test('When half-open, a fail with a filtered error', t => {
       t.ok(breaker.opened, 'should be open after initial fire');
       t.notOk(breaker.pendingClose,
         'should not be pending close after initial fire');
-    });
 
-  // Fire again after reset timeout. should be half open
-  setTimeout(() => {
-    t.ok(breaker.halfOpen, 'should be halfOpen after timeout');
-    t.ok(breaker.pendingClose, 'should be pending close after timeout');
-    breaker
-      .fire(400) // fail with a filtered error
-      .catch(e =>
-        t.equals(e.message, 'Failed with 400 status code', 'should fail again'))
-      .then(() => {
-        t.ok(breaker.opened,
-          'should be opened after failing with filtered error');
-      })
-      .then(_ => breaker.shutdown())
-      .then(t.end);
-  }, options.resetTimeout * 1.5);
+      // Fire again after reset timeout. should be half open
+      setTimeout(() => {
+        t.ok(breaker.halfOpen, 'should be halfOpen after timeout');
+        t.ok(breaker.pendingClose, 'should be pending close after timeout');
+        breaker
+          .fire(400) // fail with a filtered error
+          .catch(e =>
+            t.equals(e.message, 'Failed with 400 status code',
+              'should fail again'))
+          .then(() => {
+            t.ok(breaker.closed,
+              'should be closed after failing with filtered error');
+          })
+          .then(_ => breaker.shutdown())
+          .then(t.end);
+      }, options.resetTimeout * 1.5);
+    });
 });
