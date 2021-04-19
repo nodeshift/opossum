f6a37b4f0d736426ce1699adcb4ec7ac75246021
Lucas Holmquist <lholmqui@redhat.com>
2021-04-19 11:22:29 -0400

    squash: remove repeated test

--- test/test.js
+++ test/test.js
@@ -475,64 +475,6 @@ test('CircuitBreaker status', t => {
     });
 });
 
-test('CircuitBreaker status - import stats', t => {
-  t.plan(12);
-  const breaker = new CircuitBreaker(passFail, { errorThresholdPercentage: 1 });
-  const prevStats = {
-    failures: 10,
-    fallbacks: 0,
-    successes: 5,
-    rejects: 0,
-    fires: 15,
-    timeouts: 0,
-    cacheHits: 0,
-    cacheMisses: 0,
-    semaphoreRejections: 0,
-    percentiles: {},
-    latencyTimes: []
-  };
-
-  breaker.importStatus(prevStats);
-  const deepEqual = (t, expected) =>
-    actual => t.deepEqual(actual, expected, 'expected status values');
-
-  Promise.all([
-    breaker.fire(10).then(deepEqual(t, 10)),
-    breaker.fire(20).then(deepEqual(t, 20)),
-    breaker.fire(30).then(deepEqual(t, 30))
-  ])
-    .then(() => t.deepEqual(breaker.status.stats.fires, 3,
-      'breaker fired 3 times'))
-    .catch(t.fail)
-    .then(() => {
-      breaker.fire(-10)
-        .then(t.fail)
-        .catch(value => {
-          const stats = breaker.status.stats;
-          t.equal(value, 'Error: -10 is < 0',
-            'fails with correct error message');
-          t.equal(stats.failures, 1, 'status reports a single failure');
-          t.equal(stats.fires, 4, 'status reports 4 fires');
-        })
-        .then(() => {
-          breaker.fallback(() => 'Fallback called');
-          return breaker.fire(-20)
-            .then(result => {
-              const stats = breaker.status.stats;
-              t.equal(result, 'Fallback called', 'fallback is invoked');
-              t.equal(stats.failures, 1, 'status reports 1 failure');
-              t.equal(stats.rejects, 1, 'status reports 1 reject');
-              t.equal(stats.fires, 5, 'status reports 5 fires');
-              t.equal(stats.fallbacks, 1, 'status reports 1 fallback');
-            })
-            .catch(t.fail);
-        })
-        .then(_ => breaker.shutdown())
-        .catch(t.fail)
-        .then(t.end);
-    });
-});
-
 test('CircuitBreaker rolling counts', t => {
   const opts = { rollingCountTimeout: 200, rollingCountBuckets: 2 };
   const breaker = new CircuitBreaker(passFail, opts);
