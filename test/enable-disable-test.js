e2cb181f8486925105981d448a1a4c1e7d310965
Gautam Jethwani <gjethwani@atlassian.com>
2023-09-12 02:02:17 -0700

    feat: add semicolon

--- test/enable-disable-test.js
+++ test/enable-disable-test.js
@@ -78,12 +78,14 @@ test('Event listener should be removed only for the breaker that is disabled', t
   const breakerToBeDisabled = new CircuitBreaker(passFail, {
     rotateBucketController: emitter
   });
-  const _breakerNotToBeDisabled = new CircuitBreaker(passFail, {
+  const breakerNotToBeDisabled = new CircuitBreaker(passFail, {
     rotateBucketController: emitter
   });
   t.equals(emitter.listeners('rotate').length, 2, '1 listener attached for each breaker');
   breakerToBeDisabled.disable();
   t.equals(emitter.listeners('rotate').length, 1, '1 listener should be disabled and 1 should remain');
+  breakerToBeDisabled.shutdown();
+  breakerNotToBeDisabled.shutdown();
   t.end();
 });
 
@@ -98,5 +100,36 @@ test('Event listener should be re-added when circuit is re-enabled', t => {
   t.equals(emitter.listeners('rotate').length, 0, 'listener removed when breaker disabled');
   breaker.enable();
   t.equals(emitter.listeners('rotate').length, 1, 'listener re-attached when breaker re-enabled');
+  breaker.shutdown();
+  t.end();
+});
+
+test('Listener should not be attached with a call to enable if there is already a listener', t => {
+  t.plan(2);
+  const emitter = new EventEmitter();
+  const breaker = new CircuitBreaker(passFail, {
+    rotateBucketController: emitter
+  });
+  t.equals(emitter.listeners('rotate').length, 1, 'listener attached automatically');
+  breaker.enable();
+  t.equals(emitter.listeners('rotate').length, 1, 'listener should not be added again');
+  breaker.shutdown();
+  t.end();
+});
+
+test('Listener should not be attached with a call to enable if there is already a listener and there is another breaker in the mix', t => {
+  t.plan(2);
+  const emitter = new EventEmitter();
+  const breaker = new CircuitBreaker(passFail, {
+    rotateBucketController: emitter
+  });
+  const anotherBreaker = new CircuitBreaker(passFail, {
+    rotateBucketController: emitter
+  });
+  t.equals(emitter.listeners('rotate').length, 2, 'listener attached automatically');
+  breaker.enable();
+  t.equals(emitter.listeners('rotate').length, 2, 'listener should not be added again');
+  breaker.shutdown();
+  anotherBreaker.shutdown();
   t.end();
 });
