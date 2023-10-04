244a89ce26f2e2586ead51e5a00ac6bb6076bc26
Lucas Holmquist <lholmqui@redhat.com>
2023-10-04 11:42:25 -0400

    fix: fix bug where options couldn't be set to zero.
    when setting things like resetTimeout and errorThresholdPercentage to 0, the default value was being used.
    
    fixes #824

--- test/test.js
+++ test/test.js
@@ -57,6 +57,74 @@ test('uses name as a group when no group is provided', t => {
   t.end();
 });
 
+test('errorThresholdPercentage defaults to 50', t => {
+  t.plan(1);
+  const breaker = new CircuitBreaker(passFail);
+  t.equals(breaker.options.errorThresholdPercentage, 50, 'should be set to 50 by default');
+  breaker.shutdown();
+  t.end();
+});
+
+test('errorThresholdPercentage can be set to 0', t => {
+  t.plan(1);
+  const breaker = new CircuitBreaker(passFail,
+    { errorThresholdPercentage: 0 });
+  t.equals(breaker.options.errorThresholdPercentage, 0, 'should be set to 0');
+  breaker.shutdown();
+  t.end();
+});
+
+test('resetTimeout defaults to 30000', t => {
+  t.plan(1);
+  const breaker = new CircuitBreaker(passFail);
+  t.equals(breaker.options.resetTimeout, 30000, 'should be set to 30000 by default');
+  breaker.shutdown();
+  t.end();
+});
+
+test('resetTimeout can be set to 0', t => {
+  t.plan(1);
+  const breaker = new CircuitBreaker(passFail,
+    { resetTimeout: 0 });
+  t.equals(breaker.options.resetTimeout, 0, 'should be set to 0');
+  breaker.shutdown();
+  t.end();
+});
+
+test('rollingCountTimeout defaults to 10000', t => {
+  t.plan(1);
+  const breaker = new CircuitBreaker(passFail);
+  t.equals(breaker.options.rollingCountTimeout, 10000, 'should be set to 10000 by default');
+  breaker.shutdown();
+  t.end();
+});
+
+test('rollingCountTimeout can be set to 0', t => {
+  t.plan(1);
+  const breaker = new CircuitBreaker(passFail,
+    { rollingCountTimeout: 0 });
+  t.equals(breaker.options.rollingCountTimeout, 0, 'should be set to 0');
+  breaker.shutdown();
+  t.end();
+});
+
+test('rollingCountBuckets defaults to 10', t => {
+  t.plan(1);
+  const breaker = new CircuitBreaker(passFail);
+  t.equals(breaker.options.rollingCountBuckets, 10, 'should be set to 10 by default');
+  breaker.shutdown();
+  t.end();
+});
+
+test('rollingCountBuckets can be set to 0', t => {
+  t.plan(1);
+  const breaker = new CircuitBreaker(passFail,
+    { rollingCountBuckets: 0 });
+  t.equals(breaker.options.rollingCountBuckets, 0, 'should be set to 0');
+  breaker.shutdown();
+  t.end();
+});
+
 test('Passes parameters to the circuit function', t => {
   t.plan(1);
   const expected = 34;
